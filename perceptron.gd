51149ca79c0da46c5e2d61bd2db255a476716b5c
Greaby <contact@greaby.com>
2019-06-26 07:34:28 +0200

    feat(perceptron): add compute and train functions

--- perceptron.gd
+++ perceptron.gd
@@ -1,6 +1,8 @@
 extends Node
 class_name Perceptron
 
+const learning_rate = 0.01
+
 var weights = []
 
 func _init(n: int):
@@ -8,3 +10,16 @@ func _init(n: int):
 	for i in range(n):
 		# random weights between -1 and 1
 		weights.push_back(randf() * 2 - 1)
+
+func compute(values : Array):
+	var result = 0
+	for i in weights.size():
+		result += weights[i] * values[i]
+	return sign(result)
+
+func train(values: Array, target: int):
+	var result = compute(values)
+	var error = target - result
+
+	for i in weights.size():
+		weights[i] = lerp(weights[i], weights[i] + error * values[i], learning_rate)
